238dea5af3608cd9594e0bf3c0d153d445e643e7
ZimmerD <davidzimmer91@gmail.com>
2018-12-12 08:27:35 +0100

    final Update Deedle Task 3_2

--- workShop_scripting/scripts/Task_3_2_Deedle.fsx
+++ workShop_scripting/scripts/Task_3_2_Deedle.fsx
@@ -16,14 +16,13 @@ open FSharpAux
 open FSharp.Stats
 open System.IO
 
-
 let rawDataPath = Path.Combine [|__SOURCE_DIRECTORY__; "../" ;"data/PeptideRatioTable.tab";|]
 
 /// Task 1: Read data & inspect the data Frame
 let rawData :Frame<int,string> = 
     Frame.ReadCsv(rawDataPath,separators="\t") 
 
-/// Task 2: Group peptides by ProtId
+/// Task 2: Group peptides by ProtId. Creating a hierachical row index (protID,rowNumber) of type (string*int).
 let groupedByProtID: Frame<string*int,string> = 
     rawData
     |> Frame.groupRowsBy "Protein"
@@ -48,20 +47,19 @@ let filterProteinsWithMissingValues =
     |> Frame.sliceCols ratioColumns
     |> Frame.filterRowValues (fun x -> x.As<float>() |> Series.countValues >= 35)
 
-/// task 7: Declare a function to calculate the mean row-wise. 
+/// Task 7: Declare a function to calculate the mean row-wise. 
 let calcMeansRowWise (columnKeys:string seq) (data: Frame<_,_>) = 
         data
         |> Frame.sliceCols columnKeys
         |> Frame.mapRowValues (fun row -> row.As<float>() |> Series.values |> FSharp.Stats.Seq.mean)
 
-///TODO:
-/// task 6: Declare a function to calculate the mean row-wise. 
+/// Task 6: Declare a function to calculate the mean row-wise. 
 let calcStdevRowWise (columnKeys:string seq) (data: Frame<_,_>) = 
         data
         |> Frame.sliceCols columnKeys
         |> Frame.mapRowValues (fun row -> row.As<float>() |> Series.values |> FSharp.Stats.Seq.stDev)
 
-/// task 8: Create mapping from the biological replicate to the technical replicates.
+/// Task 8: Create mapping from the biological replicate to the technical replicates.
 let bioRepToTechnicalReplicates = 
     ratioColumns
     |> Seq.groupBy (fun x -> 
@@ -69,7 +67,7 @@ let bioRepToTechnicalReplicates =
                         tmp.[0]
                    ) 
 
-/// task 9: Following the mapping from biological replicate to technical replicate,
+/// Task 9: Following the mapping from biological replicate to technical replicate,
 ///         Calculate the mean across technical replicates as an estimator for protein abundance
 ///         in a biological replicate.
 let aggregateTechnicalReplicates = 
@@ -97,7 +95,7 @@ let withImputedValues =
     |> Frame.indexColsWith colKeys
     |> Frame.indexRowsWith rowKeys
 
-/// task 12: Create mapping from the timepoint to the biological replicates.
+/// Task 12: Create mapping from the timepoint to the biological replicates.
 let timePointToBiologicalReplicates = 
     bioRepToTechnicalReplicates
     |> Seq.map fst
@@ -106,28 +104,56 @@ let timePointToBiologicalReplicates =
                         tmp.[0]
                    ) 
 
-
-/// task 12: Following the mapping from biological replicate to technical replicate,
-///         Calculate the mean across technical replicates as an estimator for protein abundance
-///         in a biological replicate.
+/// Task 13: Following the mapping from timepoint to biological replicate,
+///         Calculate the mean across biological replicates as an estimator for protein abundance
+///         at a timepoint.
 let meanAcrossBiologicalReplicates = 
     timePointToBiologicalReplicates 
     |> Seq.map (fun (bioRep, bioRepColKeys) -> 
                    bioRep + "_mean", calcMeansRowWise bioRepColKeys withImputedValues               
                )
     |> Frame.ofColumns
-
-///
+    
+/// Task 14: Following the mapping from timepoint to biological replicate,
+///         Calculate the standard deviation across biological replicates as an estimator for protein dispersion
+///         at a timepoint.
 let stdevAcrossBiologicalReplicates =
     timePointToBiologicalReplicates 
     |> Seq.map (fun (bioRep, bioRepColKeys) -> 
                    bioRep + "_stdev", calcStdevRowWise bioRepColKeys withImputedValues               
                )
     |> Frame.ofColumns
-///
-let joinedF = Frame.mergeAll [withImputedValues;meanAcrossBiologicalReplicates;stdevAcrossBiologicalReplicates;] 
+
+/// Task 15: Merge Frames
+let mergedF = Frame.mergeAll [withImputedValues;meanAcrossBiologicalReplicates;stdevAcrossBiologicalReplicates;] 
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-Chart.Column( FSharp.Stats.Distributions.Empirical.create bw testNAvsCA |> Map.toSeq )
-|> Chart.Show
+/// Task 16: Create a Mapping from ATG Numbers to map man bins and bin description.
+let atgToMappingAndDescription :Frame<string,string>= 
+    groupedByProtID
+    |> Frame.sliceCols ["MapManNumer";"Description"]
+    |> Frame.applyLevel Pair.get1Of2 (Series.firstValue)
 
+/// Task 17: Join map man bins and bin description to the final Frame.
+let annotatedF =
+    Frame.join JoinKind.Left mergedF atgToMappingAndDescription
+
+/// Task 18 : tTest tp "NA" vs. tp "DA24"
+let withPAndQValues = 
+    let naReps = 
+        timePointToBiologicalReplicates |> Seq.find (fun (tp,reps) -> tp = "NA") |> snd
+    let caReps = 
+        timePointToBiologicalReplicates |> Seq.find (fun (tp,reps) -> tp = "DA24") |> snd
+    let na = mergedF |> Frame.sliceCols naReps |> Frame.toArray2D |> Array2D.toJaggedArray
+    let da = mergedF |> Frame.sliceCols caReps |> Frame.toArray2D |> Array2D.toJaggedArray
+    let pValues = 
+        Array.map2 (fun nas das -> FSharp.Stats.Testing.TTest.twoSample false (nas|> vector) (das |> vector)) na da 
+        |> Array.map (fun x -> x.PValue)
+    let pi0 = 
+        FSharp.Stats.Testing.PvalueAdjust.Qvalues.pi0_Bootstrap pValues
+    let qValues = FSharp.Stats.Testing.PvalueAdjust.Qvalues.ofPValues pi0 pValues
+    annotatedF
+    |> Frame.addCol "pValues" (Series.ofObservations (Seq.zip annotatedF.RowKeys pValues))
+    |> Frame.addCol "qValues" (Series.ofObservations (Seq.zip annotatedF.RowKeys qValues))
+        
+open FSharp.Plotly
